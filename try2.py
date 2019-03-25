5efabe8d4783a54783431627922602cfa251efda
gounis13gounis <45018738+gounis13gounis@users.noreply.github.com>
2019-03-25 14:14:14 -0400

    first

---
+++ try2.py
@@ -0,0 +1,37 @@
+from pylab import *
+import pandas_datareader
+import pandas as pd
+import numpy as np
+
+
+
+def Datapull(Stock):
+    try:
+        df = (pandas_datareader.DataReader(Stock,'yahoo',start='01/01/2010'))
+        print('Retrieved', Stock)
+        time.sleep(5)
+        return df
+    except e:
+        print ('Main Loop', str(e))
+print(df)
+
+def RSIfun(price, n=14):
+    delta = price['Close'].diff()
+    #-----------
+    # dUp=delta[delta>0]
+    # dDown=delta[delta<0]
+    #
+    # RolUp=RolUp.reindex_like(delta, method="ffill")
+    # RolDown=RolDown.reindex_like(delta, method="ffill")
+    dUp, dDown =delta.copy(),delta.copy()
+    dUp[dUp<0]=0
+    dDown[dDown > 0] = 0
+    RolUp=pandas.rolling_mean(dUp,n)
+    RolDown=pandas.rolling_mean(dDown,n).abs
+    RS = RolUp / RolDown
+    rsi= 100.0 - (100.0 / (1.0 + RS))
+    return rsi
+
+Stock='AAPL'
+df=Datapull(Stock)
+RSIfun(df)
